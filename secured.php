e0f22324e1be69d78addabf053eae2c7a856765d
Dwi Ramadhan R <dwi.buss@gmail.com>
2025-06-22 13:02:30 +0700

    mengubah nilai accept dari input file, agar memfilter hanya file .png, .jpg, dan .jpeg yg ditampilkan pada window "choose file" di awal.

--- secured.php
+++ secured.php
@@ -103,7 +103,7 @@ if (isset($_POST['submit'])) {
 
             <div>
                 <label for="file-input" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Pilih file</label>
-                <input type="file" id="file-input" name="imageFile" accept="image/*" required class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400">
+                <input type="file" id="file-input" name="imageFile" accept=".png, .jpeg, .jpg" required class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400">
                 <p class="mt-1 text-xs text-gray-500 dark:text-gray-300" id="file_input_help">Hanya menerima file PNG, JPG, JPEG.</p>
             </div>
 
